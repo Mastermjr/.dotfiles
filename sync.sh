1d79f2bda6db4e0961fe44090f8912f0614feec1
master <master@localhost.localdomain>
2019-01-18 14:12:49 -0500

    fixing a few errors

--- sync.sh
+++ sync.sh
@@ -16,3 +16,10 @@ ln -s $BASE/tmux.conf ~/.tmux.conf
 #oh-my-zsh custom
 cd oh-my-zsh/
 ln -s $BASE/custom-oh-my-zsh/mikey_theme.zsh-theme custom/themes/mikey_theme.zsh-theme
+
+#add in .local_aliases file
+if [ -f ~/.local_aliases ]; then
+  exit
+else
+  touch .local_alisases
+fi
