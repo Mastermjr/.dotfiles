77e955aab4ad8d453f0f065e74626c4b90a52925
mastermjr <mastermjr@protonmail.com>
2019-01-17 22:48:11 -0500

    -n removes recursive sym link

--- sync.sh
+++ sync.sh
@@ -5,7 +5,7 @@ BASE=~/.dotfiles
 
 #vim
 ln -s $BASE/vimrc ~/.vimrc
-ln -s $BASE/vim/ ~/.vim
+ln -sn $BASE/vim/ ~/.vim
 
 #zsh
 ln -s $BASE/zshrc ~/.zshrc
@@ -15,4 +15,4 @@ ln -s $BASE/tmux.conf ~/.tmux.conf
 
 #oh-my-zsh custom
 cd oh-my-zsh/
-ln -s $BASE/custom-oh-my-zs/mikey_theme.zsh-theme custom/themes/mikey_theme.zsh-theme
+ln -s $BASE/custom-oh-my-zsh/mikey_theme.zsh-theme custom/themes/mikey_theme.zsh-theme
