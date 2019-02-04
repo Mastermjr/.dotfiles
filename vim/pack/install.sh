f1e5dc2e0c94b4ed13789d3da4c6d5f289553ff7
mastermjr <mastermjr@prontonmail.com>
2019-02-03 23:28:59 -0500

    fixing install.sh

--- vim/pack/install.sh
+++ vim/pack/install.sh
@@ -50,7 +50,7 @@ function package () {
   expected_repo=$(basename "$repo_url" .git)
   if [ -d "$expected_repo" ]; then
     cd "$expected_repo" || exit
-    result=$(git pull ) #may need --force
+    result=$(git pull --force ) #may need --force
     echo "$expected_repo: $result"
   else
     confirm "$repo_url"
